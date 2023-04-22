ac015fdee35e165a95d1127e1c45ba1137dadaeb
Anupam Shakya <69745638+theanupamshakya@users.noreply.github.com>
2023-04-22 22:29:54 +0530

    Create 1312. Minimum Insertion Steps to Make a String Palindrome.java

---
+++ 1312. Minimum Insertion Steps to Make a String Palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+public:
+  int minInsertions(string s) {
+    int n = s.length();
+    vector<vector<int>> dp(n, vector<int>(n, 0));
+
+    for(int len = 2; len <= n; len++) 
+    {
+        for (int i = 0; i <= n - len; i++) 
+        {
+            int j = i+len - 1;
+            if (s[i] == s[j]) 
+            {
+                dp[i][j] = dp[i+1][j-1];
+            } else 
+            {
+                dp[i][j] = min(dp[i+1][j], dp[i][j-1]) + 1;
+            }
+        }
+    }
+
+    return dp[0][n-1];
+}
+
+};
