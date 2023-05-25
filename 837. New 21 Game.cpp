db86717677c1c11573fd9ebb11f465f2b5e99fc1
Anupam Shakya <69745638+theanupamshakya@users.noreply.github.com>
2023-05-25 23:07:48 +0530

    Create 837. New 21 Game.cpp

---
+++ 837. New 21 Game.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    double new21Game(int n, int k, int maxPts) {
+        if(k==0 || n >= k+maxPts)
+        return 1;
+
+        vector<double>dp(n+1, 0.0);
+
+        dp[0] = 1;
+        double currSum = dp[0];
+        for(int i = 1; i<= n; i++)
+        {
+            dp[i] = currSum / (double)maxPts;
+            if(i<k)
+            {
+                currSum += dp[i];
+            }
+
+            if(i - maxPts >= 0)
+            {
+                currSum -= dp[i- maxPts];
+            }
+        }
+
+        double ans = 0;
+        for(int i = k; i<=n; i++)
+        ans+= dp[i];
+        return ans;
+    }
+};
