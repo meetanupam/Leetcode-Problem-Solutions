4f9f8e3462eb012a15f85f9a65582b52fc0b8c99
Anupam Shakya <69745638+theanupamshakya@users.noreply.github.com>
2023-05-29 23:36:33 +0530

    Create 1603. Design Parking System.cpp

---
+++ 1603. Design Parking System.cpp
@@ -0,0 +1,20 @@
+class ParkingSystem {
+ public:
+  ParkingSystem(int big, int medium, int small) {
+    count = {big, medium, small};
+  }
+
+  bool addCar(int carType) {
+    return count[carType - 1]-- > 0;
+  }
+
+ private:
+  vector<int> count;
+};
+
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
