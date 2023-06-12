8445afe36abc44cc627a3bb61626029f82aa0b40
Anupam Shakya <69745638+theanupamshakya@users.noreply.github.com>
2023-06-12 22:08:14 +0530

    Create 228. Summary Ranges.cpp

---
+++ 228. Summary Ranges.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> ranges;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int start = nums[i];
+            
+            while (i + 1 < nums.size() && nums[i] + 1 == nums[i + 1])
+            {
+                i++;
+            }
+
+            if (start != nums[i])
+            {
+                ranges.push_back(to_string(start) + "->" + to_string(nums[i]));
+            } else
+            {
+                ranges.push_back(to_string(start));
+            }
+        }
+        return ranges;
+    }
+};
